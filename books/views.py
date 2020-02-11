11649ff3f0a544b15a2d414d66dd4f1fe9e78072
Zer <zersiax@localhost.localdomain>
2020-02-11 21:57:59 +0000

    Added permissions, started on dashboards

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -5,29 +9,50 @@ from django.urls import reverse_lazy
 from .models import Book
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'book_list.html'
+    login_url = 'login'
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
 
 
-class BookDetailView(DetailView): 
+class BookDetailView(LoginRequiredMixin, DetailView): 
     model = book
     template_name = 'book_detail.html'
+    login_url = 'login'
 
 
-class BookUpdateView(UpdateView): 
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
     model = Book
     fields = ('title', 'author', 'rating')
     template_name = 'book_edit.html'
+    login_url = 'login'
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
-class BookDeleteView(DeleteView):
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Book
     template_name = 'book_delete.html'
     success_url = reverse_lazy('book_list')
+    login_url = 'login'
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class BookCreateView(CreateView):
+
+class BookCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Book
     template_name = 'book_new.html'
-    fields = ('title', 'rating', 'author', 'poster',)
-    
+    fields = ('title', 'rating', 'author',)
+    login_url = 'login'
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+    def form_valid(self, form):
+        return super().form_valid(self,form):
+        form.instance.poster = self.request.user
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView
 
+from books.models import Book
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_books'] = Book.objects.all()[:5]
+        return context
