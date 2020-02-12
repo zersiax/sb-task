2e3e50f3b64619d39bf348bdf758381249f3f650
Zer <zersiax@localhost.localdomain>
2020-02-12 10:45:40 +0000

    Misc changes

--- roodgeads_project/urls.py
+++ roodgeads_project/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('books/', include('books.urls')), #
+    path('books/', include('books.urls')),
     path('', include('pages.urls')),
     
 ]
